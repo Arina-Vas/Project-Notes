21aad2495ab59af83274ab95b9f290dc144a8c47
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-11-25 20:05:40 +0100

    version_002

--- main.js
+++ main.js
@@ -4,12 +4,33 @@ const mockNotes = [
     { id: 3, title: "Объекты (JavaScript)", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", color: "pink", isFavorite: false, isHidden: false }
 ]
 
+const messages = {
+    deleteMessage: {
+        text: 'Заметка удалена',
+        image: 'assets/images/Done.svg'
+    },
+    addMessage: {
+        image: 'assets/images/Done.svg',
+        text: 'Заметка добавлена'
+    },
+    messageWarning: {
+        image: 'assets/images/warning.svg',
+        class: 'message-warning',
+        text: 'Заполните все поля'
+    },
+    messageWarningLength: {
+        image: 'assets/images/warning.svg',
+        class: 'message-warning',
+        text: 'Максимальная длина заголовка - 50 символов'
+    }
+}
+
 const myStorage = window.sessionStorage;
 // myStorage.notes = model.notes
 const model = {
     notes: JSON.parse(myStorage.getItem('notes')) || [],
     addNote(noteTitle, noteText, noteColor) {
-        this.notes.unshift({ id: Math.random(), title: noteTitle, text: noteText, color: noteColor, isFavorite: false, isHidden: false })
+        this.notes.unshift({ id: Math.random(), title: noteTitle, text: noteText, color: noteColor, isFavorite: false })
         this.saveNotes()
     },
     deleteNote(noteId) {
@@ -25,17 +46,13 @@ const model = {
         })
         this.saveNotes()
     },
-    showFavorite() {
-        this.notes.forEach((note) => {
-            if (!note.isFavorite) {
-                note.isHidden = true;
-            }
-        })
-        this.saveNotes()
-    },
-    showAllNotes() {
-        this.notes.forEach((note) => note.isHidden = false)
-        this.saveNotes()
+    toggleShowFavorite(isShowOnlyFavorite) {
+        if (isShowOnlyFavorite) {
+            return this.notes.filter(note => note.isFavorite);
+        }
+        else{
+            return this.notes;
+        }
     },
     saveNotes() {
         myStorage.setItem('notes', JSON.stringify(this.notes));
@@ -45,7 +62,6 @@ const model = {
 const view = {
     init() {
         this.renderNotes(model.notes)
-        controller.showAllNotes();
 
         const form = document.querySelector('.form');
         const inputTitle = document.querySelector('.input-title')
@@ -53,81 +69,46 @@ const view = {
         const notesList = document.querySelector('.list')
         const favoriteListToggle = document.querySelector('.checkbox-input')
         const colorsList = document.querySelector('.colors-list')
-        let text = "";
-        let type = "";
 
         colorsList.addEventListener('click', (e) => {
             const selectedColor = colorsList.querySelector('.select-color');
             if (e.target.classList.contains('circle')) {
-                if (!selectedColor) {
-                    e.target.parentElement.classList.add('select-color')
-                }
-                else {
-                    selectedColor.classList.remove('select-color')
-                    e.target.parentElement.classList.add('select-color')
-                }
+                selectedColor.classList.remove('select-color')
+                e.target.parentElement.classList.add('select-color')
             }
         })
 
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             const selectedColor = e.target.querySelector('.select-color');
-            let noteColor = ''
-            if (selectedColor) {
-                noteColor = selectedColor.firstElementChild.id
-            }
+            const noteColor = selectedColor.firstElementChild.id
             const noteTitle = inputTitle.value;
             const noteText = inputText.value;
-            if (!noteTitle || !noteText) {
-                text = 'Заполните все поля';
-                type = 'message-warning';
-            }
-            else if (noteTitle.length > 50) {
-                type = 'message-warning';
-                text = 'Максимальная длина заголовка - 50 символов';
-                // view.showMessage(type, text);
-            }
-            else {
-                noteText.trim();
-                noteTitle.trim();
-                type = 'message';
-                text = 'Заметка добавлена';
-                controller.addNote(noteTitle, noteText, noteColor)
-                // view.showMessage(type, text);
+            const isNoteAdded = controller.addNote(noteTitle, noteText, noteColor)
+            if (isNoteAdded) {
                 inputTitle.value = '';
                 inputText.value = '';
-                if (selectedColor) {
-                    selectedColor.classList.remove('select-color')
-                }
-                favoriteListToggle.checked = false;
-                controller.showAllNotes();
             }
-            this.showMessage(type, text);
+            if (selectedColor) {
+                const yellow = document.querySelector('.yellow')
+                selectedColor.classList.remove('select-color')
+                yellow.parentElement.classList.add('select-color')
+            }
+            favoriteListToggle.checked = false;
         })
 
         favoriteListToggle.addEventListener('click', () => {
-            if (favoriteListToggle.checked) {
-                controller.showFavorite();
-            }
-            else if (!favoriteListToggle.checked) {
-                controller.showAllNotes();
-            }
-
+            const isShowOnlyFavorite = favoriteListToggle.checked;
+            controller.toggleShowFavorite(isShowOnlyFavorite)
         })
 
         notesList.addEventListener('click', (e) => {
             const noteId = +e.target.closest('li').id
             if (e.target.classList.contains('delete-button')) {
-                type = 'message';
-                text = "Заметка удалена";
-                this.showMessage(type, text);
                 controller.deleteNote(noteId)
             }
             else if (e.target.classList.contains('favorite-check')) {
                 controller.toggleFavorite(noteId);
-                if (favoriteListToggle.checked) {
-                    controller.showFavorite();
-                }
             }
         })
     },
@@ -142,8 +123,8 @@ const view = {
         }
         let notesListHTML = '';
         notes.forEach((note) => {
-            notesListHTML += `<li id = '${note.id}' class = "${note.isHidden ? 'hidden note' : 'note'}"> 
-            <div class = "note-header ${note.color ? note.color : 'yellow'}">
+            notesListHTML += `<li id = '${note.id}' class = 'note'> 
+            <div class = "note-header ${note.color}">
                 <p class = 'note-title'>${note.title}</p> 
                 <div class = 'buttons'>
                     <input class="favorite-check" type="checkbox" ${note.isFavorite ? 'checked' : 'unchecked'}> 
@@ -159,14 +140,13 @@ const view = {
     notesCounterUpdate(notes) {
         document.querySelector('.count').textContent = notes.length
     },
-    showMessage(type, text) {
+    showMessage(messageType) {
         const messageBox = document.querySelector('.message-box')
-        // messageBox.classList.add(`${type}`)
         const message = document.createElement('div');
-        message.classList.add('message', type);
+        message.classList.add('message', messageType.class);
         message.innerHTML = `
-            <img src="${type === 'message' ? 'assets/images/Done.svg' : 'assets/images/warning.svg'}" alt="warning">
-            <span>${text}</span>`
+            <img src= '${messageType.image}'>
+            <span>${messageType.text}</span>`
         messageBox.prepend(message)
         setTimeout(() => {
             message.remove()
@@ -176,24 +156,34 @@ const view = {
 
 const controller = {
     addNote(noteTitle, noteText, noteColor) {
-        model.addNote(noteTitle, noteText, noteColor);
-        view.renderNotes(model.notes)
+        noteTitle = noteTitle.trim();
+        noteText = noteText.trim();
+        if (!noteTitle || !noteText) {
+            view.showMessage(messages.messageWarning)
+            return false;
+        }
+        else if (noteTitle.length > 50) {
+            view.showMessage(messages.messageWarningLength);
+            return false;
+        }
+        else {
+            model.addNote(noteTitle, noteText, noteColor);
+            view.showMessage(messages.addMessage);
+            view.renderNotes(model.notes)
+            return true;
+        }
     },
     deleteNote(noteId) {
         model.deleteNote(noteId);
         view.renderNotes(model.notes);
+        view.showMessage(messages.deleteMessage)
     },
     toggleFavorite(noteId) {
         model.toggleFavorite(noteId)
         view.renderNotes(model.notes)
     },
-    showFavorite() {
-        model.showFavorite();
-        view.renderNotes(model.notes)
-    },
-    showAllNotes() {
-        model.showAllNotes();
-        view.renderNotes(model.notes)
+    toggleShowFavorite(isShowOnlyFavorite) {
+        view.renderNotes(model.toggleShowFavorite(isShowOnlyFavorite))
     }
 }
 
